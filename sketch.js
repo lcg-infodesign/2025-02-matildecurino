dab6f2d937fc004968110aaa56baaa7d3e15bb72
matildecurino <matilde.curino0509@gmail.com>
2025-10-17 10:10:47 +0200

    [sketch] Modifiche
    stroke, translate e rotate

--- sketch.js
+++ sketch.js
@@ -1,146 +1,112 @@
-// let table;
+//layout 
+let table; 
+let outerPadding = 20;
+let padding = 10; 
+let itemSize = 30;
+let cols;
+let rows;
+let totalHeight;
 
-// function preload() {
-//   // put preload code here
-//   table = loadTable("dataset.csv", "csv", "header");
-// }
+//scala 
+let minValueCol0;
+let maxValueCol0;
+let minValueCol1;
+let maxValueCol1;
+let minValueCol2;
+let maxValueCol2;
+let minValueCol3; 
+let maxValueCol3;
+let minValueCol4;
+let maxValueCol4;
 
-// function setup() {
-//   // controllo se ho caricato i dati 
-//   console.log(table);
-
-//   // capire la grandezza della canva 
-//   let outerPadding = 20;
-//   let padding = 10;
-//   let itemSize = 30;
-//   // calcolo il numero di colonne
-//   // arrotondo il numero di colonne per difetto --> funzione floor
-//   let cols = floor((windowWidth - outerPadding * 2) / (itemSize + padding));
-  
-
-//   // calcolo il numero di righe 
-//   // arrotondo il numero di righe per eccesso --> funzione ceil
-//   let rows = ceil(getRowCount() / cols);
-  
-
-//   let totalHeight = outerPadding * 2 + rows * itemSize + (rows - 1) * padding;
-
-//   // creo il canvas
-//   createCanvas(windowWidth, totalHeight);
-
-//   background("coral");
-
-//   console.log("cols:", cols);
-//   console.log("righe:", rows);
-
-//   let colCount = 0;
-//   for(let rowNumber = 0; rowNumber < table.getRowCount(); rowNumber++); {
-//     // console.log("riga numero:", rowNumber);
-
-//     // carica i dati dalla tabella 
-//     let data = table.getRow(rowNumber);
-//     console.log(data);
-
-//     let xPos = outerPadding + colCount * itemSize; 
-     
-//     rect(xPos, 50, itemSize, itemSize);
-//     // aumento colcount
-//     colCount++
-//   }
-// }
-
-
-
-// function draw() {
-//   // put drawing code here
-// }
-
-let table;
+//colori 
+let colorOne;
+let colorTwo;
 
 function preload() {
-  // put preload code here
-  table = loadTable("dataset.csv","csv","header");
+  table = loadTable("dataset.csv", "csv", "header");
 }
 
 function setup() {
-  //controllo se ho caricato i dati
-  console.log(table); //stampa sulla console cosa c'è nella variabile table
+  //calcolo cols and rows
+  cols = floor((windowWidth - outerPadding *2) / (itemSize + padding));
+  rows = ceil(table.getRowCount() / cols);
+  totalHeight = outerPadding * 2 + rows * itemSize + (rows -1)* padding; 
 
-  //larghezza dello sketch uguale a quella della finestra
-  // decido un ingombro (larghezza, gap, quanti in una riga...)
-  // quando so il numero di righe posso sapere l'altezza dello sketch 
+  createCanvas(windowWidth, totalHeight);
 
-  let outerPadding = 20; // padding esterno
-  let padding = 10; // padding tra gli elementi
-  let itemSize = 30; // dimensione degli elementi
+  //min and max values
+  let allValuesCol0 = table.getColumn("column0");
+  minValueCol0 = min(allValuesCol0);
+  maxValueCol0 = max(allValuesCol0);
 
-  //calcolo il numero delle colone  // colonne = larghezza - 2 * padding esterno/ item + padding interno
-  let cols = floor((windowWidth - outerPadding * 2) / (itemSize + padding)); // arrotondo per difetto --> meglio una colonna in meno che una che sborda
+  let allValuesCol1 = table.getColumn("column1");
+  minValueCol1 = min(allValuesCol1);
+  maxValueCol1 = max(allValuesCol1);
 
-  let rows = ceil(table.getRowCount() / cols); //numero di righe / il numero di colonne
+  let allValuesCol2 = table.getColumn("column2");
+  minValueCol2 = min(allValuesCol2);
+  maxValueCol2 = max(allValuesCol2);
 
-  let totalHeight = outerPadding * 2 + rows * itemSize + (rows - 1) * padding;
+  let allValuesCol3 = table.getColumn("column3");
+  minValueCol3 = min(allValuesCol3);
+  maxValueCol3 = max(allValuesCol3);
 
-  //creo il canvas
-  createCanvas(windowWidth, totalHeight); //larghezza finestra, altezza calcolata
+  let allValuesCol4 = table.getColumn("column4");
+  minValueCol4 = min(allValuesCol4);
+  maxValueCol4 = max(allValuesCol4);
 
-  background("coral");
+  //colori 
+  colorOne = color("pink");
+  colorTwo = color("green");
 
-  console.log("cols: ", cols, "rows: ", rows);
+  rectMode(CENTER);
+  angleMode(DEGREES);
 
-  let colCount = 0;
-  let rowCount = 0; 
-
-  //per leggere ogni riga
-  for(let rowNumber = 0; rowNumber < table.getRowCount(); rowNumber++){
-    //per ogni riga carico i dati dalla tabella
-    let data = table.getRow(rowNumber).obj;
-    console.log(data);
-
-    // prendo valore per dimensione 
-    let myValueCol0= data['column0'];
-
-    // devo creare una funzione di scala 
-    // calcolo il valore massimo e minimo
-    let allValuesCol0 = table.getColumn("column0");
-    let minValueCol0 = min(allValuesCol0);
-    let maxValueCol0 = max(allValuesCol0);
-    let scaledValueCol0 = map(myValueCol0, minValueCol0, maxValueCol0, 1, itemSize);
-
-    // seconda variabile per il colore 
-    let myValueCol2 = data['column2'];
-    let allValuesCol2 = table.getColumn("column2");
-    let minValueCol2 = min(allValuesCol2);
-    let maxValueCol2 = max(allValuesCol2);
-
-    //due colori
-    let colorOne = color("yellow");
-    let colorTwo = color("blue");
-    let value2Mapped = map(myValueCol2, minValueCol2, maxValueCol2, 0, 1);
-
-    //risultato del mapping
-    let mappedColor = lerpColor(colorOne, colorTwo, value2Mapped);
-    fill(mappedColor);
-
-    //posizione x
-    let xPos = outerPadding + colCount * (itemSize+padding);
-    let yPos = outerPadding + rowCount * (itemSize+padding);
-
-    ellipse(xPos, yPos, scaledValueCol0, scaledValueCol0); // x, y, largh., altez.
-
-
-    //ad ogni ciclo aumento colCount
-    colCount++; 
-
-    //controllo se siamo a fine riga
-    if(colCount == cols) {
-      colCount = 0; 
-      rowCount++; 
-    }
-  }
 }
 
-
 function draw() {
-  // put drawing code here
+  background(25);
+
+  let colCount = 0;
+  let rowCount = 0;
+
+  for (let rowNumber = 0; rowNumber < table.getRowCount(); rowNumber++) {
+    let data = table.getRow(rowNumber).obj;
+
+    let myValueCol0 = data["column0"];
+    let mappedValuesCol0 = map(myValueCol0, minValueCol0, maxValueCol0, 1, itemSize);
+
+    let myValueCol1 = data["column1"];
+    let mappedStroke = map(myValueCol1, minValueCol1, maxValueCol1, 0, 4);
+
+    let myValueCol2 = data["column2"];
+    let colorMapped = map(myValueCol2, minValueCol2, maxValueCol2, 0, 1);
+    let mappedColor = lerpColor(colorOne, colorTwo, colorMapped);
+
+    let myValueCol3 = data["column3"];
+    let mappedRotation = map(myValueCol3, minValueCol3, maxValueCol3, 0, 360);
+
+    let myValueCol4 = data["column4"];
+    let scaledValueCol4 = map(myValueCol4, minValueCol4, maxValueCol4, 0, 1);
+
+    noFill();
+    strokeWeight(mappedStroke);
+    stroke(mappedColor);
+    
+    let xPos = outerPadding + colCount * (itemSize + padding);
+    let yPos = outerPadding + rowCount * (itemSize + padding);
+
+    push();
+    translate(xPos, yPos);
+    rotate(mappedRotation);
+    rect(0, 0, mappedValuesCol0, mappedValuesCol0);
+    pop();
+
+    colCount++;
+    if (colCount == cols) {
+      colCount = 0; 
+      rowCount++;
+    }
+  }
 }
