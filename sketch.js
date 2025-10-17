92b2969856043cc71a3cc0636855bd71858d932d
matildecurino <matilde.curino0509@gmail.com>
2025-10-17 13:00:31 +0200

    [sketch] Modifiche ulteriori

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 //layout 
 let table; 
-let outerPadding = 20;
-let padding = 10; 
+let outerPadding = 70;
+let padding = 5; 
 let itemSize = 30;
 let cols;
 let rows;
@@ -20,8 +20,10 @@ let minValueCol4;
 let maxValueCol4;
 
 //colori 
-let colorOne;
-let colorTwo;
+let verdeFluo;
+let fucsiaFluo;
+let verdeFluoTrasparente;
+let fucsiaFluoTrasparente;
 
 function preload() {
   table = loadTable("dataset.csv", "csv", "header");
@@ -57,16 +59,18 @@ function setup() {
   maxValueCol4 = max(allValuesCol4);
 
   //colori 
-  colorOne = color("pink");
-  colorTwo = color("green");
-
+  verdeFluo = color(57, 255, 20);
+  fucsiaFluo = color(255, 20, 147);
+  verdeFluoTrasparente = color(57, 255, 20, 60);
+  fucsiaFluoTrasparente = color(255, 20, 147, 60);
+  
   rectMode(CENTER);
   angleMode(DEGREES);
 
 }
 
 function draw() {
-  background(25);
+  background(0);
 
   let colCount = 0;
   let rowCount = 0;
@@ -78,29 +82,63 @@ function draw() {
     let mappedValuesCol0 = map(myValueCol0, minValueCol0, maxValueCol0, 1, itemSize);
 
     let myValueCol1 = data["column1"];
-    let mappedStroke = map(myValueCol1, minValueCol1, maxValueCol1, 0, 4);
+    let mappedStrokeCol1 = map(myValueCol1, minValueCol1, maxValueCol1, 0, 4);
+    let mappedReverseStrokeCol2 = map(myValueCol1, maxValueCol1, minValueCol1, 0, 4);
 
     let myValueCol2 = data["column2"];
     let colorMapped = map(myValueCol2, minValueCol2, maxValueCol2, 0, 1);
-    let mappedColor = lerpColor(colorOne, colorTwo, colorMapped);
+    let mappedColorCol2 = lerpColor(verdeFluo, fucsiaFluo, colorMapped);
+    let shadowColorMappedCol2 = map(myValueCol2, minValueCol2, maxValueCol2, 0, 1);
+    let mappedShadowColorCol2 = lerpColor(verdeFluoTrasparente, fucsiaFluoTrasparente, shadowColorMappedCol2);
+    let mappedStrokeCol2 = map(myValueCol2, minValueCol2, maxValueCol2, 0, 6);
 
     let myValueCol3 = data["column3"];
-    let mappedRotation = map(myValueCol3, minValueCol3, maxValueCol3, 0, 360);
+    let mappedRotationCol3 = map(myValueCol3, minValueCol3, maxValueCol3, 0, 360);
+    let mappedReverseRotationCol3 = map(myValueCol3, maxValueCol3, minValueCol3, 0, 360);
+    let mappedValuesCol3 = map(myValueCol3, minValueCol3, maxValueCol3, 0, itemSize);
 
     let myValueCol4 = data["column4"];
-    let scaledValueCol4 = map(myValueCol4, minValueCol4, maxValueCol4, 0, 1);
+    let mappedValuesCol4 = map(myValueCol4, minValueCol4, maxValueCol4, 0, itemSize);
+    let mappedRotationCol4 = map(myValueCol4, minValueCol4, maxValueCol4, 0, 360);
+    let mappedReverseRotationCol4 = map(myValueCol4, minValueCol4, maxValueCol4, 360, 0);
 
-    noFill();
-    strokeWeight(mappedStroke);
-    stroke(mappedColor);
+    noFill();  
     
     let xPos = outerPadding + colCount * (itemSize + padding);
     let yPos = outerPadding + rowCount * (itemSize + padding);
 
+    //quadrati fluo
     push();
+    strokeWeight(mappedStrokeCol1);
+    stroke(mappedColorCol2);
     translate(xPos, yPos);
-    rotate(mappedRotation);
+    rotate(mappedRotationCol3);
     rect(0, 0, mappedValuesCol0, mappedValuesCol0);
+    pop();
+
+    //rettangoli fluo trasparenti
+    push();
+    strokeWeight(mappedReverseStrokeCol2);
+    stroke(mappedShadowColorCol2);
+    translate(xPos, yPos);
+    rotate(mappedRotationCol3);
+    rect(0, 0, mappedValuesCol4+20, mappedValuesCol4);
+    pop();
+
+    //archi fluo 
+    push();
+    strokeWeight(mappedReverseStrokeCol2);
+    stroke(mappedColorCol2);
+    translate(xPos, yPos);
+    arc(0, 0, mappedValuesCol4, mappedValuesCol4, mappedReverseRotationCol3, mappedRotationCol3);
+    pop();
+
+    //archi fluo trasparenti
+    push();
+    strokeWeight(mappedStrokeCol2);
+    stroke(mappedShadowColorCol2);
+    translate(xPos, yPos);
+    arc(0, 0, mappedValuesCol3, mappedValuesCol3, mappedRotationCol4, mappedReverseRotationCol4);
     pop();
 
     colCount++;
